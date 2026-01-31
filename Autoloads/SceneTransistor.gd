bd5b70728bfc5ee88e2c4c8a2d5a1db0104ed164
vh_wz <vh_wz@outlook.com>
2026-01-31 22:36:35 +0800

    [feature] 修改visable赋值时机

--- Autoloads/SceneTransistor.gd
+++ Autoloads/SceneTransistor.gd
@@ -8,12 +8,12 @@ func _ready() -> void:
     self.visible = false
 
 func start_transition_to(path_to_scene: String) -> void:
+    self.visible = true
     new_scene = path_to_scene
     animation_player.play("change_scene")
     
     
 func change_scene() -> void:
-    self.visible = true
     var __ = get_tree().change_scene(new_scene) == OK
     assert(__)
     self.visible = false
